50795eeddffc6656797e00d24ec461ceb6a4c348
Sang <huynhvansang122@gmail.com>
2021-07-01 09:33:49 +0700

    xoa nhung cai khong can thiet

--- app/src/main/java/com/example/be_hoc_toan/HocTap.kt
+++ app/src/main/java/com/example/be_hoc_toan/HocTap.kt
@@ -4,8 +4,6 @@ package com.example.be_hoc_toan
 import android.content.Intent
 
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
